df3ae1d16f1b794b0bc4b729c106a7dac2d3fc2b
Jesufemi-A <jesufemia1@gmail.com>
2024-01-04 01:33:13 +0100

    fixed extra spaces in task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let updatedTask = true;
-    let updatedTask2 = false;
-    return [updatedTask, updatedTask2];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
