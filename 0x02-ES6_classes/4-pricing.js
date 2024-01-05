17819b8c09bea271b8883d92a8b6b7f63e8c452d
Jesufemi-A <jesufemia1@gmail.com>
2024-01-05 02:53:50 +0100

    more getter and setter

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  /* amount */
+  set amount(v) {
+    if (typeof value !== 'number') {
+      throw new Error('TypeError: Amount must be a number');
+    }
+    this._amount = v;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  /* currency */
+  set currency(v) {
+    if (!(v instanceof Currency)) {
+      throw new Error('TypeError: Currency must be instance of Currency');
+    }
+    this._currency = v;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static converPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
