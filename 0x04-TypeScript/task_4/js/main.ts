a045ef2cc4a70bdc95dc4c5d09aae622fb17ac7a
Jesufemi-A <jesufemia1@gmail.com>
2024-01-11 02:04:42 +0100

    studentCOntstructor

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,29 @@
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+export default StudentClass;
